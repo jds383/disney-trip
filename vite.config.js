e4aa22aea62dcef03e05f0e99f4bd6e93e1c52d1
jds383 <starker_2hyphens@icloud.com>
2026-04-24 20:02:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/disney-trip/',
   plugins: [react()],
+  base: '/disney-trip/',
 })
