492d0245ac4717bb44421f6ffcc6a84e3cc58f82
jds383 <starker_2hyphens@icloud.com>
2026-04-26 15:22:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -299,7 +299,7 @@ const days = [
       { icon: "🍔", text: "Dining TBD — on the go", quickService: true },
       { icon: "🌟", text: "8:15 PM · Disney Starlight: Dream the Night Away Parade", url: "https://disneyworld.disney.go.com/entertainment/magic-kingdom/starlight-dream-night-away-parade/" },
       { icon: "🚤", text: "9:05 PM · Electrical Water Pageant", url: "https://disneyworld.disney.go.com/entertainment/magic-kingdom/electrical-water-pageant/" },
-      { icon: "🎆", text: "9:30 PM · Happily Ever After Fireworks", url: "https://disneyworld.disney.go.com/entertainment/magic-kingdom/happily-ever-after-fireworks/" },
+      { icon: "🎆", text: "10:00 PM · Happily Ever After Fireworks", url: "https://disneyworld.disney.go.com/entertainment/magic-kingdom/happily-ever-after-fireworks/" },
     ]
   },
   {
